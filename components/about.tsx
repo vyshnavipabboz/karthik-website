893006a9a1f0cd5dcaaa94d9f9edb6dd6e84d70a
Vyshnavi Pabboz (pvyshnav) <pvyshnav@cisco.com>
2024-02-19 22:49:10 +0530

    checking

--- components/about.tsx
+++ components/about.tsx
@@ -20,8 +20,11 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">
+          Machine learning or datascience test
+        </span>
+        , I decided to pursue my passion for programming. I enrolled in a coding
+        bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
