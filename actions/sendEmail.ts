5b6fa46c33c3881e4e72d222fd359c9e2d3c6624
vyshnavipabboz <pabbozvyshnavi.work@gmail.com>
2024-04-10 21:43:04 +0530

    updated contact, resume, github

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,8 +26,8 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Website Contact Form <onboarding@resend.dev>",
-      to: "pabbozvyshnavi.work@gmail.com",
+      from: "Karthik M Portfolio Contact <onboarding@resend.dev>",
+      to: "karthikmurumulla20@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
